b86e5cd2735459da17f107ccccef87625227fd7e
ariuka11 <ariun1118@gmail.com>
2022-01-25 12:37:34 -0500

    Dnd functionality is done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,82 @@ import initialData from "./data";
 
 const App = () => {
   const [data, setData] = useState(initialData);
-  const onDragEnd = () => {};
+
+  const onDragEnd = (result) => {
+    const { source, destination, draggableId, type } = result;
+
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    if (type === "column") {
+      const newColumnOrder = Array.from(data.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newData = {
+        ...data,
+        columnOrder: newColumnOrder,
+      };
+      setData(newData);
+      return;
+    }
+    const home = data.columns[source.droppableId];
+    const foreign = data.columns[destination.droppableId];
+
+    if (home === foreign) {
+      const newTaskIds = Array.from(home.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newHome = {
+        ...home,
+        taskIds: newTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newHome.id]: newHome,
+        },
+      };
+
+      setData(newData);
+      return;
+    }
+
+    // moving from one list to another
+
+    const homeTaskIds = Array.from(home.taskIds);
+    homeTaskIds.splice(source.index, 1);
+    const newHome = {
+      ...home,
+      taskIds: homeTaskIds,
+    };
+
+    const foreignTaskIds = Array.from(foreign.taskIds);
+    foreignTaskIds.splice(destination.index, 0, draggableId);
+    const newForeign = {
+      ...foreign,
+      taskIds: foreignTaskIds,
+    };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newHome.id]: newHome,
+        [newForeign.id]: newForeign,
+      },
+    };
+    setData(newData);
+  };
   return (
     <div className="container">
       <Navbar />
